c418750ce12f8e7f06a4150ee028cb3623990fab
Yagafarov <dinmuhammadyagafarov@gmail.com>
2024-04-11 22:51:24 +0500

    tuzatildi

--- src/components/ShareButton/index.js
+++ src/components/ShareButton/index.js
@@ -6,8 +6,8 @@ const ShareButton = () => {
     navigator
       .share({
         title: document.title,
-        text: 'Check out this quiz app — it rocks!',
-        url: 'https://safdarjamal.github.io/quiz-app/',
+        text: 'Shunchaki test Tariqasida yaratildi!',
+        url: 'https://anodra.uz/',
       })
       .then(() => console.log('Successfully shared'))
       .catch(error => console.log(error.message));
@@ -17,7 +17,7 @@ const ShareButton = () => {
     <Fragment>
       {navigator.share ? (
         <Button
-          title="Share"
+          title="Ulashish"
           floated="right"
           size="big"
           circular
@@ -30,7 +30,7 @@ const ShareButton = () => {
           size="tiny"
           trigger={
             <Button
-              title="Share"
+              title="Ulashish"
               floated="right"
               size="big"
               circular
@@ -41,7 +41,7 @@ const ShareButton = () => {
           <Modal.Header className="ui center aligned">Share on</Modal.Header>
           <Modal.Content className="ui center aligned container">
             <a
-              href="https://www.facebook.com/sharer.php?u=https%3A//safdarjamal.github.io/quiz-app/"
+              href="https://www.facebook.com"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -51,7 +51,7 @@ const ShareButton = () => {
               </Button>
             </a>
             <a
-              href="https://twitter.com/intent/tweet?url=https%3A//safdarjamal.github.io/quiz-app/&text=Check%20out%20this%20quiz%20app%20—%20it%20rocks!&via=_safdarjamal"
+              href="https://twitter.com"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -61,7 +61,7 @@ const ShareButton = () => {
               </Button>
             </a>
             <a
-              href="https://www.linkedin.com/shareArticle?mini=true&url=https%3A%2F%2Fsafdarjamal.github.io%2Fquiz-app%2F"
+              href="https://www.linkedin.com"
               target="_blank"
               rel="noopener noreferrer"
             >
